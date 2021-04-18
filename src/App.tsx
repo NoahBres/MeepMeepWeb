f402a6ebdad582400c50339dc60462b7e7f52958
Noah Bresler <noahbresler2002@gmail.com>
2021-04-17 22:26:29 -0500

    Setup tailwind and remove default templatings

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-import { Test } from "roadrunnerjs";
+// import { trajectory } from "roadrunnerjs";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // useEffect(() => {
+  //   // const t = new trajectory.TrajectoryBuilder()
+  // }, []);
 
-  useEffect(() => {}, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  return <main className="w-full h-screen bg-blue-500"></main>;
 }
 
 export default App;
