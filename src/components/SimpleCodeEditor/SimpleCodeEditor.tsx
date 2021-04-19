f982c4f6ae33ba05b4553c899625b61b21e71c5b
Noah Bresler <noahbresler2002@gmail.com>
2021-04-18 23:55:02 -0500

    Move SimpleCodeEditor to its own folder

--- src/components/SimpleCodeEditor/SimpleCodeEditor.tsx
+++ src/components/SimpleCodeEditor/SimpleCodeEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-import tokenize, { Token } from "../parser/tokenizer";
+import tokenize, { Token } from "../../parser/tokenizer";
 
 import styles from "./SimpleCodeEditor.module.css";
 
